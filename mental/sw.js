3c5f99e356ef74f5231e8171f7b81b06ba7cdf57
jperon <cataclop@hotmail.com>
2020-11-11 00:10:51 +0100

    Update mental/index.html

--- mental/sw.js
+++ mental/sw.js
@@ -1,56 +1,50 @@
-var CACHE = 'Calcul mental-pwa';
-var FILES = [
+const CACHE = 'Calcul mental-pwa';
+const FILES = [
   './',
   './index.html',
   './fonts/KaTeX_Main-BoldItalic.woff2',
   './fonts/KaTeX_Main-Bold.woff2',
   './fonts/KaTeX_Main-Italic.woff2',
-  './fonts/KaTeX_Main-Regular.woff2'
+  './fonts/KaTeX_Main-Regular.woff2',
+  './icon.png',
+  './favicon.ico'
 ];
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {
   console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
+  // Open a cache and use `addAll()` with an array of assets to add all of them
+  // to the cache. Ask the service worker to keep installing until the
+  // returning promise resolves.
+  evt.waitUntil(caches.open(CACHE).then(function (cache) {
+    cache.addAll(FILES);
+  }));
 });
 
 // On fetch, use cache but update the entry with the latest contents
 // from the server.
 self.addEventListener('fetch', function(evt) {
   console.log('The service worker is serving the asset.');
-  // Try network and if it fails, go for the cached copy.
-  evt.respondWith(fromNetwork(evt.request, 400).catch(() => {
-    return fromCache(evt.request);
-  }));
+  // You can use `respondWith()` to answer ASAP...
+  evt.respondWith(fromCache(evt.request));
+  // ...and `waitUntil()` to prevent the worker to be killed until
+  // the cache is updated.
+  evt.waitUntil(update(evt.request));
 });
-
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
-async function precache() {
-  const cache = await caches.open(CACHE);
-  return await cache.addAll(FILES);
-}
-
-// Time limited network request. If the network fails or the response is not
-// served before timeout, the promise is rejected.
-function fromNetwork(request, timeout) {
-  return new Promise((fulfill, reject) => {
-    // Reject in case of timeout.
-    var timeoutId = setTimeout(reject, timeout);
-    // Fulfill in case of success.
-    fetch(request).then((response) => {
-      clearTimeout(timeoutId);
-      fulfill(response);
-    // Reject also if network fetch rejects.
-    }, reject);
-  });
-}
 
 // Open the cache where the assets were stored and search for the requested
 // resource. Notice that in case of no matching, the promise still resolves
 // but it does with `undefined` as value.
 async function fromCache(request) {
   const cache = await caches.open(CACHE);
-  const matching = await cache.match(request);
-  return matching || Promise.reject('no-match');
+  return await cache.match(request);
+}
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+async function update(request) {
+  const cache = await caches.open(CACHE);
+  const response = await fetch(request);
+  await cache.put(request, response.clone());
+  return response;
 }
