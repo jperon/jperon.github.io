e263b58b6ad75fce571b7c340dacb18c6e90096b
jperon <cataclop@hotmail.com>
2023-10-12 20:12:11 +0200

    Key field

--- grist/sql/index.py
+++ grist/sql/index.py
@@ -24,6 +24,7 @@ def create_table(id, data, header):
   document.getElementById(id).firstChild.replaceWith(table)
 
 
+key = document.querySelector("#key")
 query = document.querySelector("#query")
 q = document.querySelector("#q")
 reply = document.querySelector("#reply")
@@ -31,18 +32,25 @@ reply = document.querySelector("#reply")
 async def ctrl_enter(evt):
   if evt.ctrlKey and evt.which == 13:
     api = await grist.docApi.getAccessToken({"readOnly": True})
-    data = await fetch(
-      f"{api.baseUrl}/sql?auth={api.token}&q={query.value}"
-    ).then(lambda x: x.json())
+    opts = __new__(window.Object)
+    url = f"{api.baseUrl}/sql?q={query.value}"
+    if key.value:
+      opts.headers = __new__(window.Object)
+      opts.headers.Authorization = f"Bearer {key.value}"
+    else:
+      url += f"&auth={api.token}"
+    data = await fetch(url, opts).then(lambda x: x.json())
     if data.records:
       if data.records[0]:
         header = dict(data.records[0].fields).keys()
-        aoa = [ [
-            JSON.stringify(row.fields[fname], null, 2) for fname in header
-        ] for row in data.records ]
+        aoa = [
+          [JSON.stringify(row.fields[fname], null, 2) for fname in header]
+          for row in data.records
+        ]
         create_table('reply', aoa, header)
+        document.querySelector("#csv").value = ",".join(header) + "\n" + "\n".join([",".join(row) for row in aoa])
+        print(document.querySelector("#csv").value)
         if 'id' in header:
-          await grist.setCursorPos(1)
           await grist.setSelectedRows([row.fields['id'] for row in data.records])
       else:
         reply.innerHTML = "No result."
@@ -51,4 +59,15 @@ async def ctrl_enter(evt):
       reply.innerHTML = data.error
     evt.preventDefault()
 query.addEventListener("keydown", ctrl_enter)
+
+def download(filename, text):
+  element = document.createElement('a')
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+  element.setAttribute('download', filename)
+  element.style.display = 'none'
+  document.body.appendChild(element)
+  element.click()
+  document.body.removeChild(element)
+
+window.download = download
 
